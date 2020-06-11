59da106c6acb67333c24307e6c4b25f581989dda
Hasan KÖROĞLU <hasan@hasankoroglu.com>
2020-06-11 11:36:59 +0300

    new script

---
+++ infra/SharePoint 2016 Script/Create.new.site.collection.with.new.content.db.ps1
@@ -0,0 +1,20 @@
+#Create new site collection with new content database
+
+Add-PsSnapin "Microsoft.SharePoint.PowerShell" -EA 0
+$WebApp = "http://sp"
+$SiteName = "PortalAdi"
+$SiteDescription = "Portal Açıklama"
+$SiteUrl = "$WebApp`/sites/$SiteName"
+$SiteCollectionTemplate = "STS#0" #Classic Teams Site
+$SiteCollAdmin = "domain\user"
+$OwnerEmail = "user@domain"
+$ContentDatabaseName = "SP_$SiteName`_ContentDB"
+$ContentDatabase = New-SPContentDatabase -Name $ContentDatabaseName -WebApplication $WebApp
+#Create Site Collection
+New-SPSite -Url $SiteUrl `
+           -OwnerAlias "i:0#.w|$SiteCollAdmin" `
+           -OwnerEmail $OwnerEmail `
+           -ContentDatabase $ContentDatabase `
+           -Description $SiteDescription `
+           -Name $SiteName `
+           -Template $SiteCollectionTemplate
